179f30a8e993b6f56b9f04fb863d4f21cce1b693
pranavkhapra <pranavkhapra23@gmail.com>
2021-04-20 10:53:33 +0530

    setting up the database in firebase

--- src/base.js
+++ src/base.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase';
 import 'firebase/auth';
+import 'firebase/database';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCYXNrgSOKES-ASlEj1MCKUBp5lFZ7vWME',
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Home() {
-  return <div>Home</div>;
+  const [tweet, setTweet] = useState('');
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    setTweet({
+      [event.target.name]: event.target.value,
+    });
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="What's on your mind?."
+          maxLength={120}
+          value={tweet}
+          onChange={onChange}
+        />
+        <input type="submit" value="tweet" />
+      </form>
+    </div>
+  );
 }
 
 export default Home;
