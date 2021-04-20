f059b1cb722cf71670db62b14f292cbd915d7db6
pranavkhapra <pranavkhapra23@gmail.com>
2021-04-20 11:54:01 +0530

    deleting the tweet

--- src/components/Tweets.js
+++ src/components/Tweets.js
@@ -1,14 +1,29 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { dbService } from '../base';
 
 function Tweets({ tweetObject, isOwner }) {
+  const onDeleteClick = async () => {
+    const ok = window.confirm('Are you sure you want to delete this tweet');
+    if (ok) {
+      // delete the tweet
+      await dbService.doc(`tweets/${tweetObject.id}`).delete();
+    }
+  };
+  const onEditClick = () => {
+    //
+  };
   return (
     <div>
       <h4>{tweetObject.text}</h4>
       {isOwner && (
         <>
-          <button type="button">Delete Tweet</button>
-          <button type="button">Edit Tweet</button>
+          <button type="button" onClick={onDeleteClick}>
+            Delete Tweet
+          </button>
+          <button type="button" onClick={onEditClick}>
+            Edit Tweet
+          </button>
         </>
       )}
     </div>
